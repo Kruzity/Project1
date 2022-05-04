9934ba6c205c73e411f58e3b4e8543deeec09d39
Yan <yanolfirenko9@gmail.com>
2022-05-04 11:06:07 +0200

    Посчитайте количество вхождений слово, заданного пользователем

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,9 +1,38 @@
 #include<fstream>
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-	
+	setlocale(0, "");
+	fstream file;
+	string word;
+	cout << "¬ведите искомое слово: ";
+	getline(cin, word);
+	file.open("text.txt", ios::in);
+	if (file.is_open())
+	{
+		string tmp;
+		int c = 0;
+		while (getline(file, tmp))
+		{
+			int pos = -1;
+			while (true) {
+				pos = tmp.find(word, ++pos);
+				if (pos != string::npos) {
+					c++;
+				}
+				else break;
+			}
+		}
+		cout << word <<" найдено: " << c;
+		file.close();
+	}
+	else
+	{
+		cout << "Error" << endl;
+	}
+
 }
