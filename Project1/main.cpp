37b7a488d7b9695436cf796195b700a30b06644d
Yan <yanolfirenko9@gmail.com>
2022-05-04 11:21:39 +0200

    Количество точек и запятых

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,9 +1,35 @@
 #include<fstream>
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main()
 {
-	
+	fstream file;
+	file.open("text.txt", ios::in);
+	if (file.is_open())
+	{
+		string tmp;
+		int points = 0, comas = 0;
+		while (getline(file, tmp))
+		{
+			int point = -1, coma = -1;
+			while (true) {
+				point = tmp.find_first_of(".", ++point);
+				if (point != string::npos) {
+					points++;
+				}
+				else break;
+			}
+			while (true) {
+				coma = tmp.find_first_of(",", ++coma);
+				if (coma != string::npos) {
+					comas++;
+				}
+				else break;
+			}
+		}
+		cout << "Points: " << points <<"\nComas: "<<comas <<"\nTotal:"<<points+comas<< endl;
+	}
 }
